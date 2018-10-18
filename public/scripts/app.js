25237c6e9cd93ea411a028f1ca143f1736b2bb61
jrajan20 <jai_20j@hotmail.com>
2018-10-18 17:42:58 +0000

    database added and merged

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,9 +9,9 @@ $(() => {
   });;
 });
 
-<<<<<<< HEAD
 
-=======
+
+
 //homepage!
 //"create poll" button should show and toggle poll ()
 
@@ -26,4 +26,4 @@ $(() => {
 //results page: button  that selects our choice is a js function
 
 //final_results: should have link that can be emailed to people
->>>>>>> 00292a0ac8a50b52b18d2564b4bd401f69739a0e
+
