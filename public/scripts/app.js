1a12649a5f54cd840711cc91bc95873515aa718a
Diana Romanovsky <letsshitrainbows@gmail.com>
2018-10-18 17:19:22 +0000

    added skeleton routes to server.js file; created ejs files for the full project

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,18 @@ $(() => {
     }
   });;
 });
+
+//homepage!
+//"create poll" button should show and toggle poll ()
+
+//poll has form:
+//button adds new rows to our form
+//button deletes rows *** we are not savages
+
+//poll page: has js url selector button
+
+//vote page: shrug = js (optional)
+
+//results page: button  that selects our choice is a js function
+
+//final_results: should have link that can be emailed to people
