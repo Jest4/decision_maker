58c01771ac9158fadee4ea1afbce35de47d0eec0
jrajan20 <jai_20j@hotmail.com>
2018-10-20 21:18:25 +0000

    create_polls fixed, and data-helper for create poll updated with db insertions

--- public/scripts/create_poll.js
+++ public/scripts/create_poll.js
@@ -1,18 +1,20 @@
-	let poll_data = {
-	choice_title:[],
-	choice_desc:[]
-}
+
 
 function addChoices(){
 return	`<section class = 'choices_box'>
 				<input class='choice_title' type="text" required>
-				<button>Description</button>
+				<label>Description</label>
 				<textarea class= 'choice_desc'></textarea>
 				<button class="del_choice">Delete</button>
 			</section>  `
 }
 
 $(document).ready(function() {
+	let poll_data = {
+	choice_title:[],
+	choice_desc:[]
+}
+
 //Diana if you change any of these class names when you work on index.ejs, 
 //please change them here in these let variables
 let add = '.add_choice';
@@ -21,6 +23,7 @@ let $create = $('.create_poll');
 let title = ".choice_title";
 let desc =  ".choice_desc" ;
 let $form = $('.poll_container');
+let submit = '.submit_poll';
 let $choices = $('.choices_container');
 let $admin = $('.creator_email');
 let $pollname = $('.poll_title');
@@ -39,9 +42,10 @@ let $pollname = $('.poll_title');
 	    $(this).parent().remove();
 	});
 
-	$form.on('submit', function (event) {
+	$('body').on('click',submit, function (event) {
 		event.preventDefault();
 		$(this).filter('')
+	if ($admin.val() && $pollname.val() && $(title).val()){
 
 		if($admin.val()){
 			poll_data.admin_email = $admin.val();
@@ -52,33 +56,45 @@ let $pollname = $('.poll_title');
 		}
 
 		$(title).each(function( index ) {
-  		console.log( index + ": " + $( this ).val())
-  		poll_data.choice_title.push($( this ).val());
-  		console.log(poll_data.choice_title);
+		
+			console.log( index + ": " + $( this ).val())
+  			poll_data.choice_title.push($( this ).val());
+  			console.log(poll_data.choice_title);
+		
+  		
   		});
 
   		$(desc).each(function( index ) {
-  		console.log( index + ": " + $( this ).val() )
-  		poll_data.choice_desc.push($( this ).val());
-  		console.log(poll_data.choice_desc)
-  		});
   		
+  			console.log( index + ": " + $( this ).val() )
+  			poll_data.choice_desc.push($( this ).val());
+  			console.log(poll_data.choice_desc)
+  	
+  		
+  		});  		
   		console.log(poll_data);
+  		
+  		  $.post("/", poll_data)
+      	.then(function() {
+		console.log('data sent');
+      	});
 
-
+	} else{
+		alert('Please enter your email and title of the poll!');
+	}
+		
 	});
 		
-	$create.unbind('click').bind('click', function(){	
+	$('body').on('click', function(){	
 		$form.slideDown();
 		$form.show();
-
 	});
 
 });
 
-module.exports = {
-	poll_data: poll_data
-}
+// module.exports = {
+// 	poll_data: poll_data
+// }
 
 
 
